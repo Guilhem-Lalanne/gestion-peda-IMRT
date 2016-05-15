f44b3e944edf072f34ee6635f6b06eba9715d949
Mateusz <Mateusz@192.168.1.50>
2016-05-15 18:21:50 +0200

    gestion des utilisateurs - debut

--- GestionIMRT/src/vue/FenetreUsers.java
+++ GestionIMRT/src/vue/FenetreUsers.java
@@ -117,7 +117,7 @@ public class FenetreUsers extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        
+        parent.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton4ActionPerformed
 
