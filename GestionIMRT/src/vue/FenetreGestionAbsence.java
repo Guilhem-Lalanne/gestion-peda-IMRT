b54930257ed35023165dc96e5bc0a782358976fd
paul <paul@DESKTOP-UR8C81J>
2016-05-15 18:32:22 +0200

    rajout fenetreeployeur

--- GestionIMRT/src/vue/FenetreGestionAbsence.java
+++ GestionIMRT/src/vue/FenetreGestionAbsence.java
@@ -34,7 +34,7 @@ public class FenetreGestionAbsence extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLGestionAbsence.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLGestionAbsence.setText("gestion des absence");
+        jLGestionAbsence.setText("gestion des absences");
 
         jBSaisiAbsence.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jBSaisiAbsence.setText("Saisie des abscences");
@@ -50,18 +50,19 @@ public class FenetreGestionAbsence extends javax.swing.JFrame {
                 .addGap(200, 200, 200)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addComponent(jBSaisiAbsence, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jBBultinAbsence, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addGap(24, 24, 24)
-                        .addComponent(jLGestionAbsence)))
+                    .addComponent(jBBultinAbsence, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(171, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLGestionAbsence)
+                .addGap(196, 196, 196))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(55, 55, 55)
+                .addGap(56, 56, 56)
                 .addComponent(jLGestionAbsence, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(53, 53, 53)
+                .addGap(52, 52, 52)
                 .addComponent(jBSaisiAbsence, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(40, 40, 40)
                 .addComponent(jBBultinAbsence, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
