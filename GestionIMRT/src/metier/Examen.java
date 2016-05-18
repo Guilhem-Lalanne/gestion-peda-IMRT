1e9471b4f4019e2567a283122631fef7f56c77b3
paul <paul@DESKTOP-UR8C81J>
2016-05-18 13:56:41 +0200

    rajout class examen a finir

---
+++ GestionIMRT/src/metier/Examen.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+/**
+ *
+ * @author paul
+ */
+public class Examen {
+      private String idExamen;
+      private String idSalle;
+      private String idPresence;
+      private String idGroupeSurveille;
+}
